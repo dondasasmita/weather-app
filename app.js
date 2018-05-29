720ad7baac087c38b5a595c49aac673ab0e5d8a5
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-29 09:35:05 +0700

    user input args and encoding

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+//require request module to make http calls
 const request = require('request')
+//require config.js
 const config = require('./config.js')
+//load yargs module
+const yargs = require('yargs')
+//setup the arguments (address) to be passed in the command line
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'get weather on the address',
+            string: true
+        }
+    })
+    .help()
+    .argv
+//encode user input (ignoring spaces and replacing them with %20)
+const address = encodeURIComponent(argv.a)
 
 request({
-    url: `https://www.mapquestapi.com/geocoding/v1/address?key=${config.apiKey}&inFormat=kvp&outFormat=json&location=Singapore%20560422`,
+    url: `https://www.mapquestapi.com/geocoding/v1/address?key=${config.apiKey}&inFormat=kvp&outFormat=json&location=${address}`,
     json: true
 },(error,response,body) => {
     console.log(`Address : ${body.results[0].providedLocation.location}`)
