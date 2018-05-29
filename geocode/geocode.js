d90256347362a605efcddce9d295095a0cdb8b5f
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-29 10:28:24 +0700

    abstracting callbacks

---
+++ geocode/geocode.js
@@ -0,0 +1,28 @@
+//require request module to make http calls
+const request = require('request')
+//require config.js
+const config = require('../config')
+
+//function to get lat/long from mapquest api
+const geocodeAddress = (address, callback) => {
+    request({ 
+        url: `https://www.mapquestapi.com/geocoding/v1/address?key=${config.apiKey}&inFormat=kvp&outFormat=json&location=${address}`,
+        json: true
+    },(error,response,body) => {
+        if (error){
+            callback('Unable to connect to server.')
+        } else if (body.info.messages.length !== 0) {
+            callback('Unable to find location, try again.')
+        } else if (body.info.messages.length === 0){
+            callback(undefined, {
+                address: body.results[0].providedLocation.location,
+                latitude:body.results[0].locations[0].latLng.lat, 
+                longitude:body.results[0].locations[0].latLng.lng
+            })
+        }
+    })
+}
+
+module.exports = {
+    geocodeAddress
+}
