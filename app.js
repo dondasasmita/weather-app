3f6afb9d72f4c10ed49d11119ef8afa363d701d1
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-05-29 09:57:08 +0700

    error handling updates

--- app.js
+++ app.js
@@ -23,7 +23,13 @@ request({
     url: `https://www.mapquestapi.com/geocoding/v1/address?key=${config.apiKey}&inFormat=kvp&outFormat=json&location=${address}`,
     json: true
 },(error,response,body) => {
-    console.log(`Address : ${body.results[0].providedLocation.location}`)
-    console.log(`Latitude : ${body.results[0].locations[0].latLng.lat}`)
-    console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`)
+    if (error){
+        console.log('Unable to connect to server.')
+    } else if (body.info.messages.length !== 0) {
+        console.log('Unable to find location, try again.')
+    } else if (body.info.messages.length === 0){
+        console.log(`Address : ${body.results[0].providedLocation.location}`)
+        console.log(`Latitude : ${body.results[0].locations[0].latLng.lat}`)
+        console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`)
+    }
 })
